6c79b56e848d74c03c25c05096568c59c3f4cdc4
AZAZEL <aza@MacBook-Pro-de-AZAZEL-2.local>
2026-07-18 01:27:32 +0200

    Fix Bre-B key display in portalpagos.php to @LITTIO1042241503

--- portalpagos.php
+++ portalpagos.php
@@ -619,7 +619,7 @@
 
       <div id="brebPanelLlave" class="breb-panel">
         <div class="breb-key-box">
-          <div class="breb-key-val">@LITTIO1042243137</div>
+          <div class="breb-key-val">@LITTIO1042241503</div>
           <div class="breb-key-lbl">Llave Bre-B</div>
         </div>
       </div>
